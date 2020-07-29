d1dcd1fbe7997770460bb5e45d915fbc7c2919c8
ChristineAu-yeung <auye4760@mylaurier.ca>
2020-07-29 02:28:17 -0400

    added main

--- 160634760_a04.c
+++ 160634760_a04.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!\n");
-   printf("my first commit or push?");
-   printf("second push test");
-   printf("testing branch Christine");
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+	if(argc!=5)
+	{
+		printf("Need 4 values\n");
+		return -1;
+	}
+	
+	int resources[4], counter;
+	for(counter=1;counter<argc;counter++){
+		resources[counter] = atoi(argv[counter]);
+		// printf("%d\n", resources[counter]);
+	}
+
 }
