061a631684dd77e7f3bb84f2ff8fff56a55352ed
Brian <brianchan98@hotmail.com>
2020-07-29 22:23:41 -0400

    removed test commits, renamed appropriate c file, created executable

--- 171884160_160634760_a04.c
+++ 171884160_160634760_a04.c
@@ -2,7 +2,4 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!\n");
-   printf("my first commit or push?");
-   printf("second push test");
-   printf("branch testing");
 }
