d656e9c5accc989c70d78b4f159e79b538e1295b
ChristineAu-yeung <auye4760@mylaurier.ca>
2020-07-30 00:23:39 -0400

    commented

--- 160634760_a04.c
+++ 160634760_a04.c
@@ -7,6 +7,8 @@
 #include <time.h>
 #include <semaphore.h>
 
+//hello
+
 #define NUMBER_OF_CUSTOMERS 5
 #define NUMBER_OF_RESOURCES 4
 #define FILE_NAME "sample4_in.txt"
@@ -25,7 +27,7 @@ int allocated[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 int need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 int request_resources(int customer_num, int request[]); // RQ
 int release_resoucres(int customer_num, int release[]); // RL
-int safety_algo(customer_num);
+int safety_algo(int customer_num);
 int readfile(char* file_name);
 
 int main(int argc, char *argv[])
@@ -42,6 +44,13 @@ int main(int argc, char *argv[])
 		// printf("%d\n", resources[counter]);
 	}
 
+	int customercount = readfile(FILE_NAME);
+
+	Customer* customers;
+	while(customercount != 0){
+		printf("hi\n");
+		customercount--;
+	}
 	// read file
 
 	// print Max rescourses from file
@@ -51,4 +60,76 @@ int main(int argc, char *argv[])
 	// 		fgets(line, stdin); split line 	
 	//}
 
+}
+
+int readfile(char* filename) {
+
+	FILE *in = fopen(filename, "r");
+	if(!in)
+	{
+		printf("Error in opening input file...exiting with error code -1\n");
+		return -1;
+	}
+struct stat st;
+	fstat(fileno(in), &st);
+	char* fileContent = (char*)malloc(((int)st.st_size+1)* sizeof(char));
+	fileContent[0]='\0';	
+	while(!feof(in))
+	{
+		char line[100];
+		if(fgets(line,100,in)!=NULL)
+		{
+			strncat(fileContent,line,strlen(line));
+		}
+	}
+	fclose(in);
+	char* command = NULL;
+	int customerCount = 0;
+	char* fileCopy = (char*)malloc((strlen(fileContent)+1)*sizeof(char));
+	strcpy(fileCopy,fileContent);
+	command = strtok(fileCopy,"\r\n");
+	while(command!=NULL)
+	{
+		customerCount++;
+		command = strtok(NULL,"\r\n");
+	}
+
+	char* lines[customerCount];
+	command = NULL;
+	int i=0;
+	command = strtok(fileContent,"\r\n");
+	while(command!=NULL)
+	{
+		lines[i] = malloc(sizeof(command)*sizeof(char));
+		strcpy(lines[i],command);
+		i++;
+		command = strtok(NULL,"\r\n");
+	}
+
+	for(int k=0; k<customerCount; k++)
+	{
+		char* token = NULL;
+		int j = 0;
+		token =  strtok(lines[k],";");
+		while(token!=NULL)
+		{
+//this loop tokenizes each line of input file
+//write your code here to populate instances of Thread to build a collection
+			Customer customer;
+			customer.customerID = k;
+			if(j==0)
+				customer.resource1 = atoi(token);
+				printf("%s\n",token);
+				printf("%d\n",customer.resource1);
+			if(j==1)
+				customer.resource2 = atoi(token);
+			if(j==2)
+				customer.resource3 = atoi(token);
+			if(j==3)
+				customer.resource4 = atoi(token);
+			j++;
+			token = strtok(NULL,";");
+		}
+	}
+	return customerCount;
 }
